9bd109594f788e50426a2c49c3d9477cab082962
Opilane TTHK <opilane@tthk.ee>
2021-02-05 15:24:45 +0200

    Add project files.

---
+++ Pouver/Pouver/MainPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Pouver
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            var kitchen = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    BackgroundColor = Color.PeachPuff,
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Kitchen",
+                            FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),
+                            TextColor = Color.Green,
+                            HorizontalOptions = LayoutOptions.Center
+                        },
+                    }
+                }
+            };
+            var bathroom = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    BackgroundColor = Color.Blue,
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Bathroom",
+                            FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),
+                            TextColor = Color.Green,
+                            HorizontalOptions = LayoutOptions.Center
+                        },
+                    }
+                }
+            };
+            var laboratory = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    BackgroundColor = Color.Pink,
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Laboratory",
+                            FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),
+                            TextColor = Color.Green,
+                            HorizontalOptions = LayoutOptions.Center
+                        },
+                    }
+                }
+            };
+            var playroom = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    BackgroundColor = Color.Green,
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Playing room",
+                            FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),
+                            TextColor = Color.Green,
+                            HorizontalOptions = LayoutOptions.Center
+                        },
+                    }
+                }
+            };
+            var bedroom = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    BackgroundColor = Color.Yellow,
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Bedroom",
+                            FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),
+                            TextColor = Color.Green,
+                            HorizontalOptions = LayoutOptions.Center
+                        },
+                    }
+                }
+            };
+        }
+    }
+}
